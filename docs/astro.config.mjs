bd5e7599db5e279fadbb2230cfa1297664892c74
Lucas Felipe <lucasfs1007@gmail.com>
2024-03-26 15:47:21 -0300

    ajustes

--- docs/astro.config.mjs
+++
@@ -1,6 +0,0 @@
-import { defineConfig } from 'astro/config';
-
-export default defineConfig({
-  site: 'https://lucasfs1007.github.io/Compiladores/',
-  base: 'https://github.com/lucasfs1007/Compiladores',
-});
